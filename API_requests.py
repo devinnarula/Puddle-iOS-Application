60abb534bc7954c5d0239733e3b79fb6b091581d
ggailbr <ggailbr@clemson.edu>
2020-01-25 16:57:10 -0500

    Added in sorting

--- API_requests.py
+++ API_requests.py
@@ -1,17 +1,6 @@
-import requests
-import os
-from datetime import datetime, timedelta
-
-#access_token = "prj_test_pk_06ec8e7a8c4daef4b2cdab70b145c68c515431fa"
-access_token = "prj_test_sk_54ed50a4e9e77cde4b4708250e916a3ce27de10b"
-def get_json(url,params):
-    response = requests.get(
-    url,
-    params=params,
-    headers={"Authorization": "{}".format(access_token)},
-    )
-
-    json_response = response.json()
-
-    return json_response
-print(get_json("https://api.radar.io/v1/users",{}))
+rawcode = {'meta': {'code': 200, 'hasMore': False}, 'geofences': [{'_id': '5e2cb76b8be83700b72f0ebd', 'geometryCenter': {'coordinates': [34.67918715194163, -82.83663725176865], 'type': 'Point'}, 'live': False, 'enabled': True, 'description': 'Test', 'type': 'polygon', 'createdAt': '2020-01-25T21:47:23.651Z', 'geometry': {'type': 'Polygon', 'coordinates': [[[34.68216965456513, -82.84807573583319], [34.68140454618486, -82.8367726354168], [34.67935767062538, -82.83029241843673], [34.67381673639116, -82.83140821738792], [34.68216965456513, -82.84807573583319]]]}, 'geometryRadius': 774, 'tag': 'ICE', 'externalId': '164724', 'updatedAt': '2020-01-25T21:47:23.652Z'}, {'_id': '5e2cb4564ad6ac0076232873', 'geometryCenter': {'coordinates': [34.67918715194163, -82.83663725176865], 'type': 'Point'}, 'live': False, 'enabled': True, 'description': 'Test', 'type': 'polygon', 'createdAt': '2020-01-25T21:34:14.850Z', 'geometry': {'type': 'Polygon', 'coordinates': [[[34.68216965456513, -82.84807573583319], [34.68140454618486, -82.8367726354168], [34.67935767062538, -82.83029241843673], [34.67381673639116, -82.83140821738792], [34.68216965456513, -82.84807573583319]]]}, 'geometryRadius': 774, 'tag': 'ICE', 'externalId': '163416', 'updatedAt': '2020-01-25T21:34:14.851Z'}, {'_id': '5e2c9db2f4226c012eead8f1', 'geometryCenter': {'coordinates': [-82.8384410237492, 34.678585998401964], 'type': 'Point'}, 'live': False, 'enabled': True, 'description': 'Clemson Campus', 'type': 'polygon', 'createdAt': '2020-01-25T19:57:38.594Z', 'geometry': {'type': 'Polygon', 'coordinates': [[[-82.84807573583119, 34.68216965456513], [-82.8367726354168, 34.68140454618486], [-82.83029241843683, 34.67935767062518], [-82.83140821738702, 34.67381673639106], [-82.84565611167413, 34.6761813842436], [-82.84807573583119, 34.68216965456513]]]}, 'geometryRadius': 763, 'tag': 'ice', 'externalId': '145739', 'updatedAt': '2020-01-25T19:57:38.595Z'}]}
+for i in range(0,len(rawcode["geofences"])):
+    print(rawcode["geofences"][i]["geometry"]["coordinates"][0])
+    print(rawcode["geofences"][i]["tag"])
+    print(rawcode["geofences"][i]["externalId"])
+    print(rawcode["geofences"][i]["description"])
--- georeq.py
+++ georeq.py
@@ -1,6 +1,7 @@
 import requests
 import os
 from datetime import datetime, timedelta
+geospaces = {}
 access_token = "prj_test_sk_54ed50a4e9e77cde4b4708250e916a3ce27de10b"
 def get_json(url,params):
     response = requests.get(
@@ -25,14 +26,14 @@ def post_json(url,payload):
 def pull_geospace():
     return get_json('https://api.radar.io/v1/geofences',{})
 
-def upload_polygon(name,tag,coordinates): #Cordinates should be entered as an array in a string
+def upload_polygon(parameters): #Parameters in [name,tag,coordinates] strings Coordinates in [[],[],...]
     current = datetime.now()
+    name = parameters[0]
+    tag = parameters[1]
+    coordinates = parameters[2]
     payload = {'description' : name, 'type' : 'polygon', 'coordinates' : coordinates, 'tag' : tag, 'externalId' : current.strftime("%H%M%S")}
     post_json('https://api.radar.io/v1/geofences',payload)
     global geospaces
     geospaces = pull_geospace() 
-    return 
-geospaces = pull_geospace()
-print (geospaces)
-print("\n", upload_polygon("Clemson Campus","puddle","[[-82.84807573583319, 34.68216965456513], [-82.8367726354168, 34.68140454618486], [-82.83029241843673, 34.67935767062538], [-82.83140821738792, 34.67381673639116], [-82.84565611167423, 34.6761813842456], [-82.84807573583319, 34.68216965456513]]"))
-print("\n", geospaces)
+upload_polygon(['Test','ICE','[[34.68216965456513, -82.84807573583319],[34.68140454618486, -82.8367726354168],[34.67935767062538, -82.83029241843673],[34.67381673639116, -82.83140821738792],[34.68216965456513, -82.84807573583319]]'])
+print(geospaces)

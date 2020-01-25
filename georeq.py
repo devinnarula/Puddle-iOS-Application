47a49d1bdc0d802cb153e0b5646fcdba4246a4c6
ggailbr <ggailbr@clemson.edu>
2020-01-25 18:08:17 -0500

    Added in sorting

--- georeq.py
+++ georeq.py
@@ -1,6 +1,8 @@
 import requests
 import os
 from datetime import datetime, timedelta
+import json
+import boto3
 geospaces = {}
 access_token = "prj_test_sk_54ed50a4e9e77cde4b4708250e916a3ce27de10b"
 def get_json(url,params):
@@ -22,18 +24,23 @@ def post_json(url,payload):
     )
     json_response = post.json()
     return json_response
-
+def clean_geo(rawcode):
+    data={}
+    data['instances']=[]
+    for i in range(0,len(rawcode['geofences'])):
+        data['instances'].append({'Description':rawcode['geofences'][i]['description'],'Tag':rawcode['geofences'][i]['tag'],'externalId':rawcode['geofences'][i]['externalId'],'Coordinates':rawcode['geofences'][i]['geometry']['coordinates'][0]})
+    return data
 def pull_geospace():
     return get_json('https://api.radar.io/v1/geofences',{})
-
 def upload_polygon(parameters): #Parameters in [name,tag,coordinates] strings Coordinates in [[],[],...]
     current = datetime.now()
     name = parameters[0]
     tag = parameters[1]
-    coordinates = parameters[2]
+    coordinates = str(parameters[2])
     payload = {'description' : name, 'type' : 'polygon', 'coordinates' : coordinates, 'tag' : tag, 'externalId' : current.strftime("%H%M%S")}
-    post_json('https://api.radar.io/v1/geofences',payload)
+    server_output = str(post_json('https://api.radar.io/v1/geofences',payload))
+    s3 = boto3.resource('s3')
+    s3.Object('puddlebucket', 'newfile.txt').put(Body=server_output)
     global geospaces
-    geospaces = pull_geospace() 
-upload_polygon(['Test','ICE','[[34.68216965456513, -82.84807573583319],[34.68140454618486, -82.8367726354168],[34.67935767062538, -82.83029241843673],[34.67381673639116, -82.83140821738792],[34.68216965456513, -82.84807573583319]]'])
-print(geospaces)
+    geospaces = pull_geospace() #send geospaces back to client after this
+upload_polygon(['nope','geat',[[-82.84807573583119, 34.68216965456513], [-82.8367726354168, 34.68140454618486], [-82.83029241843683, 34.67935767062518], [-82.83140821738702, 34.67381673639106], [-82.84565611167413, 34.6761813842436], [-82.84807573583119, 34.68216965456513]]])
--- test.py
+++ test.py
@@ -1,10 +1,14 @@
-import json
 rawcode = {'meta': {'code': 200, 'hasMore': False}, 'geofences': [{'_id': '5e2cb76b8be83700b72f0ebd', 'geometryCenter': {'coordinates': [34.67918715194163, -82.83663725176865], 'type': 'Point'}, 'live': False, 'enabled': True, 'description': 'Test', 'type': 'polygon', 'createdAt': '2020-01-25T21:47:23.651Z', 'geometry': {'type': 'Polygon', 'coordinates': [[[34.68216965456513, -82.84807573583319], [34.68140454618486, -82.8367726354168], [34.67935767062538, -82.83029241843673], [34.67381673639116, -82.83140821738792], [34.68216965456513, -82.84807573583319]]]}, 'geometryRadius': 774, 'tag': 'ICE', 'externalId': '164724', 'updatedAt': '2020-01-25T21:47:23.652Z'}, {'_id': '5e2cb4564ad6ac0076232873', 'geometryCenter': {'coordinates': [34.67918715194163, -82.83663725176865], 'type': 'Point'}, 'live': False, 'enabled': True, 'description': 'Test', 'type': 'polygon', 'createdAt': '2020-01-25T21:34:14.850Z', 'geometry': {'type': 'Polygon', 'coordinates': [[[34.68216965456513, -82.84807573583319], [34.68140454618486, -82.8367726354168], [34.67935767062538, -82.83029241843673], [34.67381673639116, -82.83140821738792], [34.68216965456513, -82.84807573583319]]]}, 'geometryRadius': 774, 'tag': 'ICE', 'externalId': '163416', 'updatedAt': '2020-01-25T21:34:14.851Z'}, {'_id': '5e2c9db2f4226c012eead8f1', 'geometryCenter': {'coordinates': [-82.8384410237492, 34.678585998401964], 'type': 'Point'}, 'live': False, 'enabled': True, 'description': 'Clemson Campus', 'type': 'polygon', 'createdAt': '2020-01-25T19:57:38.594Z', 'geometry': {'type': 'Polygon', 'coordinates': [[[-82.84807573583119, 34.68216965456513], [-82.8367726354168, 34.68140454618486], [-82.83029241843683, 34.67935767062518], [-82.83140821738702, 34.67381673639106], [-82.84565611167413, 34.6761813842436], [-82.84807573583119, 34.68216965456513]]]}, 'geometryRadius': 763, 'tag': 'ice', 'externalId': '145739', 'updatedAt': '2020-01-25T19:57:38.595Z'}]}
 def clean_geo(rawcode):
-    data={}
-    data['instances']=[]
+    coordinates = []
+    tag = []
+    externalId = []
+    description = []
     for i in range(0,len(rawcode['geofences'])):
-        data['instances'].append({'Description':rawcode['geofences'][i]['description'],'Tag':rawcode['geofences'][i]['tag'],'externalId':rawcode['geofences'][i]['externalId'],'Coordinates':rawcode['geofences'][i]['geometry']['coordinates'][0]})
+        coordinates = coordinates + rawcode['geofences'][i]['geometry']['coordinates'][0]
+        tag = tag + rawcode['geofences'][i]['tag']
+        externalId = externalId + rawcode['geofences'][i]['externalId']
+        description = description + rawcode['geofences'][i]['description']
+    data = {'coordinates' : coordinates, 'tag' : tag, 'externalId' : externalId, 'description' : description}
     return data
-with open('data.txt', 'w') as outfile:
-    json.dump(clean_geo(rawcode), outfile)
+print(clean_geo(rawcode))
